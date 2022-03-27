feddc6677fac4b9f08bca74467323d8e5357f526
chatnatee <chatnatee_khamlue@hotmail.com>
2022-03-27 14:05:07 -0400

    .map

--- JS_function/index2.js
+++ JS_function/index2.js
@@ -22,4 +22,25 @@ function f2() {
 }
 
 f2();
-//const arrayOfNumber = [];
+
+const arrayOfNumbers = [10, 20, 30, 40, 50]; 
+let results = [];
+
+// TO DO : multiply each element by two and add it to result 
+
+for(let i = 0; i < arrayOfNumbers.length; i++){
+
+    results.push (arrayOfNumbers[i] * 2);
+
+    //resukt[] = arrayOfNumbers[0] = 10 * 2
+
+}
+
+console.log(arrayOfNumbers);
+console.log(results);
+
+//Things with .map() -> is a higher order function used on arrays..;
+
+const result2 = arrayOfNumbers.map((element) => {return element * 2;})
+
+console.log(`Obtained from map() : ${result2}`);
